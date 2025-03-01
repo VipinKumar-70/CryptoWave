1c11d338fc2d631a9de0c28ed94235c9cd94656b
VipinKumar-70 <vk20140074250@gmail.com>
2025-03-01 16:56:33 +0530

    initial file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <div className="bg-linear-to-b from-gray-700 via-gray-800 to-gray-900 px-16 py-4 text-white">
-        <Home />
-      </div>
-    </>
+    <div className="min-h-screen bg-gradient-to-b from-gray-700 via-gray-800 to-gray-900 text-white">
+      <Home />
+    </div>
   );
 }
- 
+
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,44 @@
 import React from "react";
-
+import { GoArrowUpRight } from "react-icons/go";
+import bitcoin from "../assets/bitcoin.png";
 function Navbar() {
   return (
     <>
-      <header>
-        <nav className="flex justify-between">
+      <header className="px-24 shadow-md font-serif">
+        <nav className="flex justify-between items-center py-6">
           <div>
-            <a href="/">
+            <a href="/" className="text-3xl font-bold flex items-center gap-2">
+              <img src={bitcoin} alt="cryptowave logo" className="w-8 h-8" />
               CrytoWave
-              <img src="" alt="" />
             </a>
           </div>
+
+          {/* Navigation links */}
           <ul className="flex gap-x-10">
             {["Home", "Features", "Pricing", "Blog"].map((item, index) => (
-              <a href="">
-                <li key={index}>{item}</li>
-              </a>
+              <li key={index}>
+                <a className="transition-all hover:text-gray-400" href="">
+                  {item}
+                </a>
+              </li>
             ))}
           </ul>
-          <div>
-            <select name="" id="">
+
+          {/* Currency Selector & Signup Button */}
+          <div className="flex items-center gap-x-2">
+            <select
+              name="currency"
+              id="currency"
+              className="border-2 p-1 rounded"
+            >
               <option value="INR">INR</option>
               <option value="USD">USD</option>
               <option value="EUR">EUR</option>
             </select>
-            <button>Signup</button>
+
+            <button className="flex items-center gap-2 bg-white text-black text-[14px] text-center px-6 py-2 rounded-full font-medium">
+              Sign up <GoArrowUpRight className="text-lg font-bold" />
+            </button>
           </div>
         </nav>
       </header>
